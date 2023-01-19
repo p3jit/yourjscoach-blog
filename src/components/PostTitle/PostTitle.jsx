e77e0c5436f1f6c896a27f6301d286889465047a
p3jit <prithi.das.007@gmail.com>
2023-01-20 01:35:38 +0530

    update change

--- src/components/PostTitle/PostTitle.jsx
+++ src/components/PostTitle/PostTitle.jsx
@@ -5,9 +5,9 @@ const PostTitle = ({ data }) => {
   const { isDarkMode } = useContext(DarkModeProvider);
   return (
     <div
-      className={`font-black uppercase ${
+      className={`font-black capitalize ${
         isDarkMode ? "text-gray-800" : "text-white"
-      } text-3xl md:text-7xl pb-2 md:pb-1 text-center`}
+      } text-4xl md:text-7xl pb-2 md:pb-1 text-center`}
     >
       {data}
     </div>
