34fd2303720e147325fae3ee091960fe97405da4
p3jit <prithi.das.007@gmail.com>
2023-01-28 00:03:44 +0530

    updated changes

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -24,7 +24,7 @@ const PostCard = ({ data }) => {
     >
       <div className="flex justify-between itme">
         <div
-          className={`font-bold leading-6 text-xl w-9/12 md:text-2xl ${
+          className={`font-bold leading-6 text-xl w-9/12 md:text-2xl tracking-tight ${
             !isDarkMode ? "text-slate-200" : "text-black"
           }`}
         >
@@ -38,8 +38,8 @@ const PostCard = ({ data }) => {
           {formatDate(new Date(data.timeStamp))}
         </h2>
       </div>
-      <div className="flex flex-col md:flex-row md:justify-between md:items-center pt-1">
-        <div className="flex gap-1 md:gap-2 flex-wrap">
+      <div className="flex flex-col pt-1 md:flex-row md:justify-between md:items-center">
+        <div className="flex flex-wrap gap-1 md:gap-2">
           {data.tags.map((singleTag) => {
             return <Tag key={singleTag} data={singleTag} />;
           })}
@@ -49,14 +49,14 @@ const PostCard = ({ data }) => {
         </h3>
       </div>
       <p
-        className={`break-words font-medium text-black pt-2 text-sm md:text-base ${
+        className={`break-words font-medium text-black pt-2 text-lg md:text-xl ${
           !isDarkMode ? "text-slate-300" : "text-slate-500"
         }`}
       >
-        {data.description.slice(0, 400) + "..." || data.description}
+        {data.description.slice(0, 280) + "..." || data.description}
       </p>
       <button
-        className="rounded p-2 mt-3 bg-slate-300 hover:bg-slate-400 text-slate-600 font-medium"
+        className="p-2 mt-4 font-medium rounded bg-slate-300 hover:bg-slate-400 text-slate-600"
         onClick={handleReadMore}
       >
         Read more
